2a206254fca769f1e560ea12c07658bc272d13cb
Eachann <eachannwen@outlook.com>
2023-03-29 23:43:57 +0800

    Go back to time.sleep

--- webtools/rolling.py
+++ webtools/rolling.py
@@ -1,20 +1,25 @@
 
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from scrapy.selector import Selector
 
 from webparse.steam_review_parse import steam_helper
 
-driver = webdriver.Chrome()
-
-# Crawl Target
-driver.get('https://steamcommunity.com/app/990080/reviews/?browsefilter=toprated&snr=1_5_100010_')
-
 # User Configuration
+infinity_search = 0
 target_page = 10  # 100 pages contain about 1000 reviews
 waiting_time = 1  # range of waiting in each time
-tolerated_times = 30  # number of tolerant times for each page
+tolerated_times = 60*10  # number of tolerant times for each page
+
+# Initialize
+driver = webdriver.Chrome()
+# Crawl Target
+driver.implicitly_wait(waiting_time)
+driver.get('https://steamcommunity.com/app/990080/reviews/?browsefilter=toprated&snr=1_5_100010_')
 
 # Start Crawling
 page_counter = 1
@@ -22,8 +27,10 @@ current_tolerated = tolerated_times
 while True:
 
     try:
-        driver.implicitly_wait(waiting_time)
-        driver.find_element(By.ID, 'page' + str(page_counter))
+        curr_id = 'page' + str(page_counter)
+        time.sleep(waiting_time)
+        driver.find_element(By.ID, curr_id)
+        print(curr_id)
     except NoSuchElementException:
         # Not find
         # - rolling -> to avoid too much rolling - since one window maybe contain several page block
@@ -41,7 +48,8 @@ while True:
             current_tolerated = tolerated_times
         # Page counter + 1
         page_counter += 1
-        if page_counter == target_page:
+        if (page_counter == target_page) & (infinity_search == 0):
+            print('Finish searching.')
             break
 
 # Start Parsing
