55dc3491f26256694a56ca2278d5cb6a721cdb21
Eachann <eachannwen@outlook.com>
2023-03-30 18:45:59 +0800

    Better version

--- webtools/rolling.py
+++ webtools/rolling.py
@@ -3,8 +3,9 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, JavascriptException, WebDriverException
 from scrapy.selector import Selector
 
 from webparse.steam_review_parse import steam_helper
@@ -12,23 +13,22 @@ from webparse.steam_review_parse import steam_helper
 # User Configuration
 infinity_search = 1
 target_page = 10  # 100 pages contain about 1000 reviews
-waiting_time = 1  # range of waiting in each time
-tolerated_times = 60*10  # number of tolerant times for each page
+waiting_time = 5  # minute(s)
 
 games_dict = {
-    'Hogwarts Legacy': 'https://steamcommunity.com/app/990080/reviews/?browsefilter=toprated&snr=1_5_100010_',
-    'Forza Horizon 5': 'https://steamcommunity.com/app/1551360/reviews/?browsefilter=toprated&snr=1_5_100010_',
-    'Cities: Skylines': 'https://steamcommunity.com/app/255710/reviews/?browsefilter=toprated&snr=1_5_100010_',
-    'Atomic Heart': 'https://steamcommunity.com/app/668580/reviews/?browsefilter=toprated&snr=1_5_100010_',
-    'It Takes Two': 'https://steamcommunity.com/app/1426210/reviews/?browsefilter=toprated&snr=1_5_100010_',
+    # 'Hogwarts Legacy': 'https://steamcommunity.com/app/990080/reviews/?browsefilter=toprated&snr=1_5_100010_',
+    # 'Forza Horizon 5': 'https://steamcommunity.com/app/1551360/reviews/?browsefilter=toprated&snr=1_5_100010_',
+    # 'Cities: Skylines': 'https://steamcommunity.com/app/255710/reviews/?browsefilter=toprated&snr=1_5_100010_',
+    # 'Atomic Heart': 'https://steamcommunity.com/app/668580/reviews/?browsefilter=toprated&snr=1_5_100010_',
+    # 'It Takes Two': 'https://steamcommunity.com/app/1426210/reviews/?browsefilter=toprated&snr=1_5_100010_',
     'Sid Meier’s Civilization VI': 'https://steamcommunity.com/app/289070/reviews/?browsefilter=toprated&snr=1_5_100010_',
     'Red Dead Redemption 2': 'https://steamcommunity.com/app/1174180/reviews/?browsefilter=toprated&snr=1_5_100010_',
     'Grand Theft Auto V': 'https://steamcommunity.com/app/271590/reviews/?browsefilter=toprated&snr=1_5_100010_',
-    'Overcooked! 2': 'https://steamcommunity.com/app/728880/reviews/?browsefilter=toprated&snr=1_5_100010_',
-    'Monster Hunter: World': 'https://steamcommunity.com/app/582010/reviews/?browsefilter=toprated&snr=1_5_100010_',
-    'Tomb Raider': 'https://steamcommunity.com/app/203160/reviews/?browsefilter=toprated&snr=1_5_100010_',
-    'Cyberpunk 2077': 'https://steamcommunity.com/app/1091500/reviews/?browsefilter=toprated&snr=1_5_100010_',
-    'LEGO Star Wars: The Skywalker Saga': 'https://steamcommunity.com/app/920210/reviews/?browsefilter=toprated&snr=1_5_100010_'
+    # 'Overcooked! 2': 'https://steamcommunity.com/app/728880/reviews/?browsefilter=toprated&snr=1_5_100010_',
+    # 'Monster Hunter: World': 'https://steamcommunity.com/app/582010/reviews/?browsefilter=toprated&snr=1_5_100010_',
+    # 'Tomb Raider': 'https://steamcommunity.com/app/203160/reviews/?browsefilter=toprated&snr=1_5_100010_',
+    # 'Cyberpunk 2077': 'https://steamcommunity.com/app/1091500/reviews/?browsefilter=toprated&snr=1_5_100010_',
+    # 'LEGO Star Wars: The Skywalker Saga': 'https://steamcommunity.com/app/920210/reviews/?browsefilter=toprated&snr=1_5_100010_'
 }
 
 for games_title in games_dict:
@@ -36,19 +36,42 @@ for games_title in games_dict:
     # Initialize
     driver = webdriver.Chrome()
     # Crawl Target
-    driver.implicitly_wait(waiting_time)
     driver.get(games_dict[games_title])
 
     # Start Crawling
+    wait = 0.1
+    tolerated_times = 60 * waiting_time / wait  # number of tolerant times for each page
     page_counter = 1
     current_tolerated = tolerated_times
     while True:
 
         try:
             curr_id = 'page' + str(page_counter)
-            time.sleep(waiting_time)
+            time.sleep(wait)
+
+            # Find see more content
+            try:
+                see_more_click = driver.find_element(By.XPATH, '//div[@id="GetMoreContentBtn"]')
+            except NoSuchElementException:
+                pass
+            except WebDriverException:
+                print('WebDriverException')
+                break
+            else:
+                try:
+                    if see_more_click.is_displayed():
+                        more_click = driver.find_element(By.XPATH, '//div[@id="GetMoreContentBtn"]/a')
+                        try:
+                            ActionChains(driver).click(more_click).perform()
+                            print('Click for more content', 'Page:', page_counter)
+                        except ElementNotInteractableException:
+                            pass
+                except JavascriptException:
+                    print('JavascriptException')
+                    break
+
             driver.find_element(By.ID, curr_id)
-            print(curr_id)
+            # print(curr_id)
         except NoSuchElementException:
             # Not find
             # - rolling -> to avoid too much rolling - since one window maybe contain several page block
@@ -59,6 +82,7 @@ for games_title in games_dict:
                 continue
             else:
                 # Reach the end or Network break
+                print('Reach the end or Network break')
                 break
         else:
             # Find new page, reset tolerated counter
--- webtools/viewer.py
+++ webtools/viewer.py
@@ -9,6 +9,6 @@ total_length = 0
 for file in os.listdir(csv_path):
     df = pd.read_csv(os.path.join(csv_path, file), index_col=0, header=0)
     total_length += len(df)
-    print(df)
+    print(file.split('.')[0], ':', len(df))
 
 print('Total length:', total_length)
